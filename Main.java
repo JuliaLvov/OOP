b2417d808fc83567f97686e9c8ada269ff056fd1
Julia_Lvova <juliasmirnova3@mail.ru>
2023-03-05 20:38:21 +0300

    added code

---
+++ Main.java
@@ -0,0 +1,24 @@
+package OOP.HM1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Буся");
+        Person person = new Person("Хозяйка");
+
+
+        // System.out.println(cat);
+        // System.out.println(person);
+        // System.out.println(person.voice());
+        // System.out.println(cat.voice());
+        person.call(cat);
+        System.out.println(person.getInfo());
+        System.out.println(cat.getInfo());
+        cat.getCaress(person.caress(3));
+    
+        System.out.println(person.getInfo());
+        System.out.println(cat.getInfo());
+
+
+
+    }
+}
